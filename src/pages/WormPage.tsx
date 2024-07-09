980ca6fa8241db821703530929de5ac3b09f1c2f
Neil MacLean <nbmac13@gmail.com>
2024-07-09 12:42:00 -0500

    updated text

--- src/pages/WormPage.tsx
+++ src/pages/WormPage.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import TypewriterEffect from '../components/TypewriterEffect';
 
 const WormPage: React.FC = () => {
-  const baseSentence = "Cabinated freedom path bleakly goes, tumbling, wildly along with complete yada yada yada yada yada yada yada for hopeful impact. ";
+  const baseSentence = "cabinated freedom path bleakly goes, tumbling, wildly along with complete yada yada yada yada yada yada yada for hopeful impact with concerned citizen invested only in the accounts given by folks living here years earlier while deep toff rolls about within a luxurious bin made there by frequencies which that who whom escaped control and went on to annihilate separate and regulated persons in direct commission with traders previously believed worthy and active in their attempts to partition areas located beyond normal and familiar grounds of restriction. ";
   const [fullText, setFullText] = useState(baseSentence);
   const [visibleText, setVisibleText] = useState('');
   const containerRef = useRef<HTMLDivElement>(null);
