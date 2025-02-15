bbfb8af25b6013025f8947548a476d6dc1586cdb
Neil MacLean <nbmac13@gmail.com>
2025-02-15 14:51:15 -0600

    Remove old pages that won't get used

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { GameStateProvider } from './contexts/GameStateContext';
-// import HomePage from './pages/HomePage';
-// import JournalPage from './pages/JournalPage';
-// import RootPage from './pages/RootPage';
-// import WormPage from './pages/WormPage';
-// import DotMatrix from './components/DotMatrix';
 import SaveIcon from './components/SaveIcon/SaveIcon';
 import LandingPage from './pages/LandingPage';
-// import OldFriendChatPage from './pages/OldFriendChatPage';
 import ChatProvider from './contexts/ChatContext';
-// import HedgeEntrancePage from './pages/HedgeEntrancePage';
 import NotFoundPage from './pages/404Page';
 
 function App() {
@@ -30,12 +23,6 @@ function App() {
                 <Routes>
                   <Route path="/" element={<LandingPage />} /> {/* re-add setIsVisible to Landing page */}
                   <Route path="*" element={<NotFoundPage />} />
-                  {/* <Route path="/game" element={<HomePage setIsVisible={setIsVisible} />} />
-                  <Route path="/hedgepath" element={<HedgeEntrancePage />} />
-                  <Route path="/root" element={<RootPage setIsTappable={setIsTappable} />} />
-                  <Route path="/worm" element={<WormPage />} />
-                  <Route path="/journal" element={<JournalPage />} /> */}
-                  {/* <Route path="/oldfriend" element={<OldFriendChatPage />} /> */}
                 </Routes>
               </div>
             </div>
